cd3a0d91491c91f4a8ccf5992af20abf04abcb1c
miguel garcia <miguel.gareva@gmail.com>
2024-12-03 11:28:02 +0100

    ex4, lvl1, little correction on function parameters

--- 1.2.4_php_basics_nivell1.php
+++ 1.2.4_php_basics_nivell1.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-function Contar(int $incremento, $numFinal = 10) : void {
+function Contar(int $incremento, int $numFinal = 10) : void {
 
     for ($i=0; $i <= $numFinal ; $i += $incremento) { 
         echo $i . "<br>";
